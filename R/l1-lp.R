8e7e1e6f6c6063da8d1f25e59e0b67e1a6a62c5c
agisga <alexej.go@gmail.com>
2017-03-18 14:37:55 -0500

    the difference between l1 and lp norms

---
+++ R/l1-lp.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(ggplot2)
+library(magick)
+
+# generate an equidistant grid in the X-Y-plane
+u <- seq(-5, 5, by = 0.01)
+m  <- length(u)
+xy <- matrix(c(rep(u, each = m), rep(u, m)), ncol = 2)
+colnames(xy) <- c("x", "y")
+xy <- as_data_frame(xy)
+
+# evaluate the fused penalty on the grid, draw points with value <=1 only, and save a figure for each value of `a`
+a <- 0.6
+p <- seq(1, 10, by = 1)
+for(i in 1:length(p)) {
+  xy %>%
+    mutate( Value = (abs(x) + abs(y)) - a * (abs(x)^p[i] + abs(y)^p[i])^(1/p[i]) ) %>%
+    filter(abs(Value) <= 1) %>%
+    ggplot(aes(x, y, color = Value)) + geom_point(pch = ".") +
+    annotate("text", x = -2.5, y = 2.5, parse = TRUE, size = 6,
+             color = "blue", label = paste0("p == ", p[i])) +
+    xlab(expression(beta[1])) + ylab(expression(beta[2])) +
+    xlim(-5, 5) + ylim(-5, 5) + scale_color_continuous(limits = c(0, 1)) +
+    ggtitle(expression((group("|", beta[1], "|") + group("|", beta[2], "|")) - 0.6 * sqrt(group("|", beta[1], "|")^p + group("|", beta[2], "|")^p, 1/p) <= 1)) +
+    theme_minimal()
+  ggsave(paste0("../img/l1-lp_img/", sprintf("%03.0f", i), ".jpeg"),
+         width = 10, height = 8, units = "cm")
+}
+
+# make a GIF
+images <- paste0("../img/l1-lp_img/", list.files(path = "../img/l1-lp_img/", pattern = "jpeg"))
+frames <- c()
+for (i in length(images):1) {
+  x <- images[i] %>% image_read() %>% image_scale("500")
+  frames <- c(x, frames)
+}
+animation <- image_animate(frames, fps = 2)
+image_write(animation, "../gif/l1-lp_balls.gif")
